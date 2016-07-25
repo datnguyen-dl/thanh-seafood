35433ada87d8113c9d33435b33be190740a57ed8
tandat <somattien@gmail.com>
2016-07-25 23:41:09 +0700

    [UI] Modify ui

--- core/database/connect.php
+++ core/database/connect.php
@@ -3,7 +3,7 @@
 $localhost_admin	=	'localhost';
 $username_admin		=	'root';
 $password_admin		=	'';
-$database_admin		=	'login_register';
+$database_admin		=	'thanh-seafood';
 
 $connect_error = 'Sorry, We\'re experiencing connection proplems.';
 $connect = mysql_connect($localhost_admin, $username_admin, $password_admin) or die ($connect_error);
